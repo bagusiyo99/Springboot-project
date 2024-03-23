3419fed9cecc0c10558ec50fddb9c7d76ad00a58
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2024-03-23 14:46:02 +0700

    update database lagi

--- src/main/java/com/mantap/SpringStarter/Controller/HomeController.java
+++ src/main/java/com/mantap/SpringStarter/Controller/HomeController.java
@@ -8,14 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home (Model model){
         return "home";
     }
 
-        @GetMapping("/about")
-    public String about (Model model){
-        return "about";
-    }
+  
 
 }
