3c5e737d241156a7b3fab30d846585773d900697
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2024-03-24 21:01:35 +0700

    data

--- src/main/java/com/mantap/SpringStarter/Controller/HomeController.java
+++ src/main/java/com/mantap/SpringStarter/Controller/HomeController.java
@@ -1,18 +1,27 @@
 package com.mantap.SpringStarter.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.mantap.SpringStarter.models.Post;
+import com.mantap.SpringStarter.services.PostService;
+
 
 @Controller
 public class HomeController {
-//route home
+    @Autowired
+    private PostService postService;
+
     @GetMapping("/")
-    public String home (Model model){
+    public String home(Model model){
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
         return "home";
     }
-
   
-
+    
 }
