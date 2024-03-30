df90e1268e438ba1260c9cc439b05a091afcbaea
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2024-03-30 09:13:51 +0700

    login

--- src/main/java/com/mantap/SpringStarter/config/WebSecurityConfig.java
+++ src/main/java/com/mantap/SpringStarter/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -22,11 +24,32 @@ public class WebSecurityConfig {
     };
 
     @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
-            .authorizeRequests()
-                .anyRequest()
-                .authenticated();
+        .authorizeRequests()
+       
+        .anyRequest()
+        .authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .loginProcessingUrl("/login")
+        .usernameParameter("email")
+        .passwordParameter("password")
+        .defaultSuccessUrl("/", true)
+        .failureUrl("/login?error")
+        .permitAll()
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .logoutSuccessUrl("/logout?success")
+        .and()
+        .httpBasic();
 
         //TODO: remove these after upgrading the DB from H2 infile DB
         http.csrf().disable();
